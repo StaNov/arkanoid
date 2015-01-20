c63028a75f421acf4d9d4f296b899e7bfc126d41
Standa Novak <stanovec@gmail.com>
2015-01-20 21:57:53 +0100

    fixed small ball speed

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -11,8 +11,18 @@ public class BallScript : MonoBehaviour {
 		soundPlayer = GameObject.Find("SoundPlayer").GetComponent<SoundPlayerScript>();
 		rigidbody2D.AddForce (new Vector2 (ballSpeed, ballSpeed));
 	}
-
+	
 	void OnCollisionEnter2D(Collision2D other) {
 		soundPlayer.PlayBallHitsWall();
+	}
+	
+	void OnCollisionExit2D(Collision2D other) {
+		float velX = rigidbody2D.velocity.x;
+		float velY = rigidbody2D.velocity.y;
+
+		if (Mathf.Abs(velX) > Mathf.Abs(velY)) {
+			rigidbody2D.velocity = Vector2.zero;
+			rigidbody2D.AddForce(new Vector2(ballSpeed * velX/Mathf.Abs(velX), ballSpeed * velY/Mathf.Abs(velY)));
+		}
 	}
 }
