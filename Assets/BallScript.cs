9bdfdd3dd402e1fd1abb81408c69b5c3f7f2ccc3
Standa Novak <stanovec@gmail.com>
2015-01-21 20:24:25 +0100

    speed modification moved to FixedUpdate function

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class BallScript : MonoBehaviour {
 
-	public float ballSpeed = 400;
+	public float ballSpeed = 10;
 
 	private SoundPlayerScript soundPlayer;
 
+	private bool modifyVelocityAfterHit = false;
+
 	void Start () {
 		soundPlayer = GameObject.Find("SoundPlayer").GetComponent<SoundPlayerScript>();
-		rigidbody2D.AddForce (new Vector2 (ballSpeed, ballSpeed));
+		rigidbody2D.AddForce (new Vector2 (ballSpeed, ballSpeed), ForceMode2D.Impulse);
 	}
 	
 	void OnCollisionEnter2D(Collision2D other) {
@@ -17,12 +19,23 @@ public class BallScript : MonoBehaviour {
 	}
 	
 	void OnCollisionExit2D(Collision2D other) {
+		modifyVelocityAfterHit = true;
+	}
+
+	void FixedUpdate() {
+
+		if (! modifyVelocityAfterHit) {
+			return;
+		}
+
+		modifyVelocityAfterHit = false;
+
 		float velX = rigidbody2D.velocity.x;
 		float velY = rigidbody2D.velocity.y;
-
+		
 		if (Mathf.Abs(velX) > Mathf.Abs(velY)) {
 			rigidbody2D.velocity = Vector2.zero;
-			rigidbody2D.AddForce(new Vector2(ballSpeed * velX/Mathf.Abs(velX), ballSpeed * velY/Mathf.Abs(velY)));
+			rigidbody2D.AddForce(new Vector2(ballSpeed * velX/Mathf.Abs(velX), ballSpeed * velY/Mathf.Abs(velY)), ForceMode2D.Impulse);
 		}
 	}
 }
