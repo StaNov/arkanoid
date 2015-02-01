bf377a85074e48bde120f403896c12efd587eb6a
StaNov <stanovec@gmail.com>
2015-02-01 13:17:55 +0100

    added hard brick + structure refactoring

--- Assets/scripts/BrickScript.cs
+++ Assets/scripts/BrickScript.cs
@@ -6,18 +6,30 @@ public class BrickScript : MonoBehaviour {
 	private GameState gameState;
 	private SoundPlayerScript soundPlayer;
 
+	public int hitPoints = 1;
+	private int remainingHits;
+
 	void Start() {
 		gameState = GameObject.Find ("GameState").GetComponent<GameState> ();
 		soundPlayer = GameObject.Find ("SoundPlayer").GetComponent<SoundPlayerScript> ();
+		remainingHits = hitPoints;
 	}
 
 	void OnCollisionEnter2D(Collision2D other) {
+		remainingHits--;
+
+		if (remainingHits == 0) {
+			DestroyBrick();
+		}
+	}
+
+	private void DestroyBrick() {
 		GetComponent<MeshRenderer> ().enabled = false;
 		GetComponent<BoxCollider2D> ().enabled = false;
 		BrickParticleSystem particleSystem = GetComponentInChildren<BrickParticleSystem> ();
 		particleSystem.PlayAndDestroy();
 		soundPlayer.PlayBallHitsBrick();
-		Destroy(gameObject);
 		gameState.OnBrickDestroyed();
+		Destroy(gameObject);
 	}
 }
