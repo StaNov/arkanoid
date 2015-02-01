6c1a6929fb96964e18251f63802440165f11eb2d
StaNov <stanovec@gmail.com>
2015-02-01 20:47:26 +0100

    added spells

---
+++ Assets/SpellCaster.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellCaster : MonoBehaviour {
+
+	public GameObject spellAutoBottom;
+	public BallSpawnerScript ballSpawner;
+	
+	public void SpawnAutoBottom() {
+		Instantiate(spellAutoBottom);
+	}
+	
+	public void SpawnNewBall() {
+		ballSpawner.SpawnBall();
+	}
+}
---
+++ Assets/scripts/SpellAutoBottom.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellAutoBottom : MonoBehaviour {
+
+	public int lifetime = 5;
+
+	private int remainingLifetime;
+	
+	void Start() {
+		remainingLifetime = lifetime;
+
+		StartCoroutine(SubtractSecond());
+	}
+
+	IEnumerator SubtractSecond() {
+
+		while (remainingLifetime > 0) {
+			yield return new WaitForSeconds(1);
+			remainingLifetime--;
+		}
+	
+		Destroy(gameObject);
+	}
+}
